89fa9bd16e22896a477ae4b87ef99c0c6727615c
Johannes Sahlmann <Johannes.Sahlmann@esa.int>
2025-01-21 10:38:04 +0100

    add license headers

--- gaia_ad/plots/plot_feature_importance.py
+++ gaia_ad/plots/plot_feature_importance.py
@@ -1,3 +1,9 @@
+#   Copyright (c) European Space Agency, 2025.
+#
+#   This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#   is part of this source code package. No part of the package, including
+#   this file, may be copied, modified, propagated, or distributed except according to
+#   the terms contained in the file ‘LICENCE.txt’.
 from loguru import logger
 from matplotlib import pyplot as plt
 import pandas as pd
--- tests/plotting_test.py
+++ tests/plotting_test.py
@@ -1,3 +1,9 @@
+#   Copyright (c) European Space Agency, 2025.
+#
+#   This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#   is part of this source code package. No part of the package, including
+#   this file, may be copied, modified, propagated, or distributed except according to
+#   the terms contained in the file ‘LICENCE.txt’.
 """Tests for the plotting functions."""
 
 from gaia_ad.plots.plot_anomaly_scores import plot_anomaly_scores
